3677560a8b4778d7e0c91674c8558f3ebfab50a5
hampet1 <hamrozipetr@outlook.com>
2022-01-08 11:50:14 +1100

    new tests added

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,8 @@
-from app import index
+from app import index, home
 
 # testing what returns index function
 def test_index():
     assert index() == "hello, world!"
 
+def test_home():
+    assert home() == "home directory"
